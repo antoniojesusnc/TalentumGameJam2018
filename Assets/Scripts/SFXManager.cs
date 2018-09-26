ee8bb087a867e7148311c123cec91a8ba055d7b6
amddm <antoniomddm@hotmail.com>
2018-09-26 21:25:47 +0200

    SoundManager y SFXManager

---
+++ Assets/Scripts/SFXManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXManager : Singleton<SFXManager>
+{
+    public AudioSource espetoCookPlus;
+    public AudioSource espetoCookWasted;
+    public AudioSource espetoDragDrop;
+    public AudioSource ringOrder;
+    public AudioSource ringOrderLost;
+
+
+    // Use this for initialization
+    void Awake () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void PlayCookPlus()
+    {
+        espetoCookPlus.Play();
+    }
+
+    void PlayCookWasted()
+    {
+        espetoCookWasted.Play();
+    }
+
+    void PlayDragDrop()
+    {
+        espetoDragDrop.Play();
+    }
+
+    void playRingOrder()
+    {
+        ringOrder.Play();
+    }
+
+    void PlayRingOrderLost()
+    {
+        ringOrderLost.Play();
+    }
+}
